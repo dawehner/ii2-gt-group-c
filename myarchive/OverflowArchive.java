bcedb869bbcc067acb7fcbe02a2f4ec11baedc3b
Daniel Wehner <dawehner@googlemail.com>
2009-06-23 23:41:48 +0200

    Overflow Archive+Test erstellt

--- myarchive/OverflowArchive.java
+++ myarchive/OverflowArchive.java
@@ -6,8 +6,15 @@ import wsiarchive.*;
 // Archiv, bei dem erst ein Archiv vollgeschrieben wird, dann das nächste etc.
 public class OverflowArchive implements IArchive {
 
+    String name; // Name des Archives
+
     // Die Archive werden der Reihe nach gefüllt
     public OverflowArchive(String name, IArchiveList archives) {
+        this.name = name;
     }
 
+    // Name des Archivs liefern
+    public String getName() {
+        return this.name;
+    }
 }
